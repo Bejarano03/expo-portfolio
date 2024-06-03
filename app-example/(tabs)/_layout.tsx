6adb3586c109ff5856fc7259f06b39ca53d65f0f
Bejarano03 <marco.bejarano03@gmail.com>
2024-06-02 23:12:32 -0700

    reset the project to work with a clean template

--- app-example/(tabs)/_layout.tsx
+++ app-example/(tabs)/_layout.tsx
@@ -24,9 +24,9 @@ export default function TabLayout() {
         }}
       />
       <Tabs.Screen
-        name="explore"
+        name="homepage"
         options={{
-          title: 'Explore',
+          title: 'HomePage',
           tabBarIcon: ({ color, focused }) => (
             <TabBarIcon name={focused ? 'code-slash' : 'code-slash-outline'} color={color} />
           ),
