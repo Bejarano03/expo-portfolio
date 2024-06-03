bcdb555edf41255a04ab9f241e9069f314447597
Bejarano03 <marco.bejarano03@gmail.com>
2024-06-02 23:40:17 -0700

    created pages and styling

--- app/contactpage.tsx
+++ app/contactpage.tsx
@@ -1,9 +1,17 @@
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet } from 'react-native'
 
 export default function ContactPage() {
     return (
-        <View>
+        <View style={styles.container}>
             <Text>Edit /app/contactpage.tsx to make changes to the page</Text>
         </View>
     );
 }
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      justifyContent: "center",
+      alignItems: "center",
+    }
+})
--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,27 @@
-import { Text, View } from "react-native";
+import { Text, View, StyleSheet } from "react-native";
+import { Link } from "expo-router";
+
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={styles.container}>
+      <Text style={styles.title}>Marco Bejarano Oseguera</Text>
+      <Text>
+        Welcome to my portfolio! Please take the time to look
+        at my project and personal achievements.
+      </Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center"
+  },
+  title: {
+    fontWeight: "bold",
+    fontSize: 44,
+  }
+})
+
